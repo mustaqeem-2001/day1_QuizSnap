d9eacb9473d7803d8ff41b242ae133c12414c21f
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2026-05-16 19:03:43 +0100

    feat: add Header, Footer, and Questions components to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./config/AppRoutes.jsx";
 import AppRoutes from "./config/AppRoutes.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <AppRoutes />
+            <Footer />
         </BrowserRouter>
     )
 }
