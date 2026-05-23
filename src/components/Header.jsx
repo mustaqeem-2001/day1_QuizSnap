e5ca2c511a6ad1d6a659b2e4aa0d4f3f72ffbf23
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2026-05-23 20:17:47 +0100

    feat/summary-screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import "@fortawesome/fontawesome-free/css/all.css";
+import { useContext } from "react";
+import { UserContext } from "../context/UserProvider";
+import { useLocation } from "react-router-dom";
 
 
 export default function Header() {
-    return (
+     const location = useLocation();
+     const { userName } = useContext(UserContext);
+     const isSummary = location.pathname === "/summary";
+
+     return (
         <header>
             <i className="fa-solid fa-brain"></i>
-            <h1 className="title">QuizSnap</h1>
-            <p className="subtitle fc-light">Test your React knowledge</p>
+            <h1 className="title">{isSummary ? "Well done, " + userName + "!" : "QuizSnap"}</h1>
+            <p className="subtitle fc-light">{isSummary ? "You've completed your questions" : "Test your React knowledge"}</p>
         </header>
     )
 }
